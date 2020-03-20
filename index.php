041f131941c1e906f99232835cb94921e6a51f88
Logan <towkrlc@gmail.com>
2020-03-20 15:29:58 +0800

    變數與常數

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
-    echo "hello world";
-    $aa;
-    $bb;
-    $cc;
-    echo "aaaaaaaaaaaaaaaa"
+    $a = 1;
+    echo $a."<br>";
+    $a = 10;
+    $b = 2;
+    $c = ($a + $b)*30;
+    echo $c."<br>";
+    echo gettype($c);
+    print($a."<br>");
+
+    const a = 50;
+    echo a."<br>";
+    define("b",3);
+    echo b."<br>";
+    
+    define("PI",3.1415926);
+    echo PI."<br>";
+    echo PI*20*20;
 ?>
