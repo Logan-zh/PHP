5ed19220fce7cf0be935c4a71b9089f3f3151a1a
Logan <towkrlc@gmail.com>
2020-03-20 16:21:12 +0800

    數值運算

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//宣告變數
     $a = 1;
     echo $a."<br>";
     $a = 10;
@@ -7,7 +8,7 @@
     echo $c."<br>";
     echo gettype($c);
     print($a."<br>");
-
+//宣告常數
     const a = 50;
     echo a."<br>";
     define("b",3);
@@ -16,4 +17,25 @@
     define("PI",3.1415926);
     echo PI."<br>";
     echo PI*20*20;
-?>
+    ?>
+    <style>
+    table{
+        border:1px solid black;
+        padding:5px;
+    }
+    td{
+        margin:20px;
+        padding:10px;
+        font-size:20px;
+    }
+    </style>
+    <table><?php
+    for($i=1 ; $i<=9; $i++){
+        echo "<tr>";
+        for($j=1; $j<=9; $j++){
+            echo "<td>".$i."×".$j."=".$i*$j."</td>";
+        };
+        echo "</tr>";
+    }?>
+    </table>
+
