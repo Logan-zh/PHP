76a40280334eb19e152893d57f73fca61842705b
Logan <towkrlc@gmail.com>
2020-03-27 16:23:56 +0800

    add web

---
+++ web/app/porfolio.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class porfolio extends Model
+{
+    protected $table = 'porfolio';
+    protected $primaryKey = 'id';
+    protected $fillable = [ 'title','content','img']
+}
