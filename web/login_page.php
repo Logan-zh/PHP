73ae474079d08d77a8cf5c7c6230c23b3532d048
Logan <towkrlc@gmail.com>
2020-05-08 09:09:46 +0800

    版面調整

--- web/login_page.php
+++ web/login_page.php
@@ -8,12 +8,14 @@
 </head>
 <body>
 <div class="container">
+<div class="sign-in-box">
+    <div class="info"><h1>
 <?php
     $showLogin = true;
     if(isset($_COOKIE['status'])){
         switch ($_COOKIE['status']){
             case 'false':
-                echo '帳號密碼錯誤';
+                echo '輸入錯誤';
             break;
             case 'NULL':
                 echo '沒有輸入';
@@ -23,13 +25,13 @@
                 $showLogin = false;
             break;
         }
-    }
-?>
+    }else{  ?>
+會員登入
+    <?php } ?>
+    </h1></div>
     <?php
     if($showLogin){
     ?>
-    <div class="sign-in-box">
-        <div class="info"><h1>會員登入</h1></div>
     <form action="login_process.php" method="POST">
         <div><span>電子信箱：</span><input type="text" name="email" value='aa@g.com'></div>
         <div><span>密　　碼：</span><input type="text" name="password" value='111111'></div>
@@ -37,12 +39,11 @@
         <a href="register.php">註冊</a>
         <input type="submit" value="登入">
         </div>
-    </form>
-    </div>
     <?php
     }else{ ?>
     <a href="logout.php" style="text-align:center"><h1>登出</h1></a>
     <?php } ?>
+    </form>
     </div>
 </body>
 </html>
--- web/logout.php
+++ web/logout.php
@@ -1,4 +1,5 @@
 <?php
-    setcookie('status',$_COOKIE['status'],time()-3600);
+    setcookie('status',null,time()-3600);
+    setcookie('id',null,time()-3600);
     header('location:login_page.php');
 ?>
