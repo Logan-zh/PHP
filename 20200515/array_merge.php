3fa3e5afda996ad5fa9c20e8d07bcfdad04d2343
Logan <towkrlc@gmail.com>
2020-05-29 15:59:40 +0800

    class

--- 20200515/array_merge.php
+++ 20200515/array_merge.php
@@ -2,11 +2,10 @@
 
     //做一個類似array_merge()的功能
     echo "<pre>";
-    print_r( merge("a","b","c","fork",52,"dock",6.5,3.88,-5,'roki') );
+    print_r( merge("a","b","c","fork",52,"dock",6.5,3.88,-5,'roki',true,false) );
 
 
     function merge(...$a){
-        $tmp = [];
         if(is_array($a)){
             foreach($a as $value){
                 $tmp[] = $value;
